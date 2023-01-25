cc21d6fcbf0746d1a16b7aed0e1897e4b49cf5d4
John <John+git@gaelicWizard.net>
2023-01-24 21:16:39 -0800

    CodePipeline: `template_file`

--- modules/codepipeline/iam.tf
+++ modules/codepipeline/iam.tf
@@ -1,9 +1,18 @@
-resource "aws_iam_role" "codepipeline" {
-  name               = "CodePipelineRole"
-  assume_role_policy = data.aws_iam_policy_document.cp_assume_role_policy.json
+resource "aws_iam_role" "KorraPipe" {
+  name               = "${var.project_name}Pipeline"
+  assume_role_policy = data.aws_iam_policy_document.KorraPipe.json
 }
 
-data "aws_iam_policy_document" "cp_assume_role_policy" {
+data "template_file" "KorraPipe" {
+  template = file("${path.module}/iam.json")
+  vars = {
+    ACCOUNT_ID         = data.aws_caller_identity.default.account_id
+    CODEBUILD_NAME     = var.project_name
+    AWS_DEFAULT_REGION = var.region
+  }
+}
+
+data "aws_iam_policy_document" "KorraPipe" {
   statement {
     actions = ["sts:AssumeRole"]
 
@@ -24,132 +33,11 @@ data "aws_iam_policy_document" "cp_assume_role_policy" {
 }
 
 
-resource "aws_iam_role_policy" "codepipelinerole_policy" {
-  name = "CodepipelineRole-Policy"
-  role = aws_iam_role.codepipeline.id
+resource "aws_iam_role_policy" "KorraPipe" {
+  name = "${var.project_name}Pipeline"
+  role = aws_iam_role.KorraPipe.id
 
-  policy = <<EOF
-{
-    "Statement": [
-        {
-            "Action": [
-                "codestar-connections:*"
-            ],
-            "Resource": "*",
-            "Effect": "Allow"
-        },
-        {
-            "Action": [
-                "s3:GetObject",
-                "s3:GetObjectVersion",
-                "s3:GetBucketVersioning"
-            ],
-            "Resource": "*",
-            "Effect": "Allow"
-        },
-        {
-            "Action": [
-                "s3:PutObject",
-                "s3:GetObject",
-                "s3:DeleteObject"
-
-            ],
-            "Resource": [
-                "arn:aws:s3:::*",
-                "arn:aws:s3:::*/*"
-            ],
-            "Effect": "Allow"
-        },
-        {
-            "Action": [
-                "codecommit:CancelUploadArchive",
-                "codecommit:GetBranch",
-                "codecommit:GetCommit",
-                "codecommit:GetUploadArchiveStatus",
-                "codecommit:UploadArchive"
-            ],
-            "Resource": "*",
-            "Effect": "Allow"
-        },
-        {
-            "Action": [
-                "codedeploy:CreateDeployment",
-                "codedeploy:GetApplicationRevision",
-                "codedeploy:GetDeployment",
-                "codedeploy:GetDeploymentConfig",
-                "codedeploy:RegisterApplicationRevision"
-            ],
-            "Resource": "*",
-            "Effect": "Allow"
-        },
-        {
-            "Action": [
-                "elasticbeanstalk:*",
-                "ec2:*",
-                "elasticloadbalancing:*",
-                "autoscaling:*",
-                "cloudwatch:*",
-                "s3:*",
-                "sns:*",
-                "cloudformation:*",
-                "rds:*",
-                "sqs:*",
-                "ecs:*",
-                "iam:PassRole"
-            ],
-            "Resource": "*",
-            "Effect": "Allow"
-        },
-        {
-            "Action": [
-                "lambda:*"
-            ],
-            "Resource": "*",
-            "Effect": "Allow"
-        },
-        {
-            "Action": [
-                "opsworks:CreateDeployment",
-                "opsworks:DescribeApps",
-                "opsworks:DescribeCommands",
-                "opsworks:DescribeDeployments",
-                "opsworks:DescribeInstances",
-                "opsworks:DescribeStacks",
-                "opsworks:UpdateApp",
-                "opsworks:UpdateStack"
-            ],
-            "Resource": "*",
-            "Effect": "Allow"
-        },
-        {
-            "Action": [
-                "cloudformation:CreateStack",
-                "cloudformation:DeleteStack",
-                "cloudformation:DescribeStacks",
-                "cloudformation:UpdateStack",
-                "cloudformation:CreateChangeSet",
-                "cloudformation:DeleteChangeSet",
-                "cloudformation:DescribeChangeSet",
-                "cloudformation:ExecuteChangeSet",
-                "cloudformation:SetStackPolicy",
-                "cloudformation:ValidateTemplate",
-                "iam:PassRole"
-            ],
-            "Resource": "*",
-            "Effect": "Allow"
-        },
-        {
-            "Action": [
-                "codebuild:BatchGetBuilds",
-                "codebuild:StartBuild"
-            ],
-            "Resource": "*",
-            "Effect": "Allow"
-        }
-    ],
-    "Version": "2012-10-17"
-}
-EOF
+  policy = data.template_file.KorraPipe.rendered
 }
 
 
