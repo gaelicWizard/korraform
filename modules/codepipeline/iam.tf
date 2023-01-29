037c84a39f9221dd2bed3c976d78fcb65c26dff4
John <John+git@gaelicWizard.net>
2023-01-28 20:13:17 -0800

    CodePipeline: IAM

--- modules/codepipeline/iam.tf
+++ modules/codepipeline/iam.tf
@@ -1,6 +1,19 @@
+/* IAM role for CodePipeline to:
+ * - Retrieve source from CodeStar connection,
+ * - Pass artifacts to/from CodeBuild & CodeDeploy,
+ * - Trigger CodeBuild,
+ * - Trigger CodeDeploy,
+ *
+ * https://aws.amazon.com/blogs/devops/complete-ci-cd-with-aws-codecommit-aws-codebuild-aws-codedeploy-and-aws-codepipeline/
+
+ */
 resource "aws_iam_role" "Korraline" {
   name               = "${var.project_name}Pipeline"
   assume_role_policy = data.aws_iam_policy_document.Korraline.json
+  /*managed_policy_arns = [
+      "arn:aws:iam::aws:policy/AWSCodeBuildDeveloperAccess",
+      "arn:aws:iam::aws:policy/AWSCodeDeployDeployerAccess"
+      ]/**/
 }
 
 data "template_file" "Korraline" {
@@ -8,6 +21,8 @@ data "template_file" "Korraline" {
   vars = {
     ACCOUNT_ID         = data.aws_caller_identity.default.account_id
     CODEBUILD_ARN      = var.codebuild_arn
+    CODEDEPLOY_ARN     = var.codedeploy_arn
+    CODESTAR_ARN       = var.codestar_arn
     AWS_DEFAULT_REGION = var.region
     S3_BUCKET          = aws_s3_bucket.Korraline.arn
   }
