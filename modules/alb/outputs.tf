5f5ba3337d2ebb6f82dfaac5f94a7ecffb5aae40
John <John+git@gaelicWizard.net>
2023-02-01 14:21:38 -0800

    ALB

--- modules/alb/outputs.tf
+++ modules/alb/outputs.tf
@@ -1,11 +1,19 @@
 output "group_arns" {
-  value = aws_lb_target_group.LoadBalancer.*.arn
+  value = aws_lb_target_group.KorraLoad.*.arn
 }
 
 output "group_names" {
-  value = aws_lb_target_group.LoadBalancer.*.name
+  value = aws_lb_target_group.KorraLoad.*.name
 }
 
 output "listener" {
-  value = aws_lb_listener.LoadBalancer.arn
+  value = aws_lb_listener.KorraLoad.arn
+}
+
+output "dns_name" {
+  value = aws_lb.KorraLoad.dns_name
+}
+
+output "zone_id" {
+  value = aws_lb.KorraLoad.zone_id
 }
