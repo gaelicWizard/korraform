03a256b80e0b33178676caedd5974cb259c613d9
John <John+git@gaelicWizard.net>
2023-02-01 17:04:25 -0800

    Task Definition

--- modules/ecs/ecs.tf
+++ modules/ecs/ecs.tf
@@ -91,8 +91,10 @@ resource "aws_ecs_service" "Korrapod" {
   }
 
   lifecycle {
-    # ignore any changes to that count caused externally (e.g., Application Autoscaling).
-    ignore_changes = [desired_count]
+    ignore_changes = [
+      desired_count,  # ignore any changes to that count caused externally (e.g., Application Autoscaling).
+      task_definition # ignore changes to task definition, which is managed by CodeDeploy
+    ]
   }
 
   /* To prevent a race condition during service deletion, make sure to set depends_on to the related aws_iam_role_policy; otherwise, the policy may be destroyed too soon and the ECS service will then get stuck in the DRAINING state.*/
