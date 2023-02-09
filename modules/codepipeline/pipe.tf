6c106facd8895db77c45b5d88fefb3c2f72a2d6e
John <John+git@gaelicWizard.net>
2023-02-09 10:58:22 -0800

    flat.

--- modules/codepipeline/pipe.tf
+++ modules/codepipeline/pipe.tf
@@ -102,7 +102,7 @@ resource "aws_codepipeline" "Korraline" {
         DeploymentGroupName = var.codedeploy_group
 
         AppSpecTemplateArtifact        = "built_image"
-        AppSpecTemplatePath            = "flatspec.yaml"
+        AppSpecTemplatePath            = "modules/codedeploy/appspec.yaml"
         TaskDefinitionTemplateArtifact = "built_image"
         TaskDefinitionTemplatePath     = "flattask.json"
 
