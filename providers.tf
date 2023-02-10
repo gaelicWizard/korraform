06253f8785833b09b38f58bdd655be667f3b8bd3
John <John+git@gaelicWizard.net>
2023-02-10 14:41:10 -0800

    Tags

--- providers.tf
+++ providers.tf
@@ -11,4 +11,11 @@ provider "aws" {
   region     = local.region
   access_key = var.aws_access_key
   secret_key = var.aws_secret_key
+
+  default_tags {
+    tags = {
+      Environment = local.environment
+      Name        = local.project_name
+    }
+  }
 }
