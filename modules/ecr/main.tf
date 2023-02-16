67638a7a91e54ee5be0230f0b0baab7457e0e593
John <John+git@gaelicWizard.net>
2023-02-16 14:42:51 -0800

    Lower-case ECR name

--- modules/ecr/main.tf
+++ modules/ecr/main.tf
@@ -1,7 +1,7 @@
 /* Elastic Container Registry
  */
 resource "aws_ecr_repository" "KorraRepo" {
-  name = var.project_name
+  name = lower(var.project_name)
   image_scanning_configuration {
     scan_on_push = true
   }
