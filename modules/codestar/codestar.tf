0555a15137b68584ff68125be65e135888b3fbb7
John <John+git@gaelicWizard.net>
2023-02-16 14:43:09 -0800

    Try to make CodeStar less likely to be destroyed…

--- modules/codestar/codestar.tf
+++ modules/codestar/codestar.tf
@@ -3,4 +3,7 @@
 resource "aws_codestarconnections_connection" "KorraStar" {
   name          = var.project_name
   provider_type = var.connection_type
+  lifecycle {
+    create_before_destroy = true
+  }
 }
